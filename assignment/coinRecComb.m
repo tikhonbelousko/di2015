330e64aa268669d2cbc154cadab988311322a976
Tikhon Belousko <dazmail01@gmail.com>
2016-01-08 20:37:09 +0300

    Add computing of total sum. Add filtering.

--- assignment/coinRecComb.m
+++ assignment/coinRecComb.m
@@ -10,10 +10,15 @@ tic;
 clear all;
 close all;
 
+% Coins
+% 0.01, 0.05, 0.10, 0.50, 1, 2, 5, 10
+COINS = [15.50 18.50 17.50 19.50 20.50 23.00 25.00 22.00; ...
+         00.01 00.05 00.10 00.50 01.00 02.00 05.00 10.00];
+
 % Load image.
 %imgrgb = imread('test_images/test_img2.jpg');
 %imgrgb = imread('iphone_images/sample5.jpg');
-imgrgb = readWithFlatField('iphone_images/sample5.jpg');
+imgrgb = readWithFlatField('iphone_images/sample1.jpg');
 
 % Convert to grayscale.
 img = rgb2gray(imgrgb);
@@ -83,11 +88,44 @@ disp(cdmm2);
 disp('Time without plotting');
 toc;
 
+% Filter outliers by mask
+percent = [];
+for ci=1:size(centers)
+    c = centers(ci,:);
+    r = radii(ci);
+    counter = 0;
+    [rows, cols] = size(imgcl);
+    for y=1:rows
+        for x=1:cols
+            if (sqrt((x - c(1))^2 + (y - c(2))^2) < r) && ...
+               (imgcl(y,x) == 0)
+                counter = counter + 1;
+            end
+        end
+    end
+    percent = [percent counter/(pi*r^2)*100];
+    disp(sprintf('N = %10.0f;\nS = %10.0f;\nP = %10.3f;\nR = %10.1f\n\n', counter, pi*r^2, counter/(pi*r^2)*100, r));
+end
+
+cs_filt = centers(percent < 5,:);
+rs_filt = radii(percent < 5);
+
 % Show original.
 imshow(imgrgb);
 
 % Visualize circles.
-viscircles(centers, radii,'EdgeColor','b');
+viscircles(cs_filt, rs_filt,'EdgeColor','b');
+
+% Total money sum
+total_sum = 0;
+for i=1:size(cdmm,1)
+    dif = abs(COINS(1,:) - cdmm(i));
+    [val, idx] = min(dif);
+    if (val < 2)
+        total_sum = total_sum + COINS(2,idx);
+    end
+end
+disp(sprintf('Total sum: %10.1f\n', total_sum));
 
 % Stopping timer.
 disp('Full time:');
